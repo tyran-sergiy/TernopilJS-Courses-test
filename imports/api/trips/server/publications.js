5a31df912a36f3c9788bf36adb60c3b7ddac40da
Tyran <turan.sergiy@gmail.com>
2017-02-23 22:51:58 +0200

    Move top Navigation to new file

--- imports/api/trips/server/publications.js
+++ imports/api/trips/server/publications.js
@@ -3,5 +3,6 @@ import { Meteor } from 'meteor/meteor';
 import { Trips } from '../trips.js';
 
 Meteor.publish('trips', function () {
+  Meteor._sleepForMs(2000);
   return Trips.find();
 });
--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,11 +2,14 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../../ui/partials/sideNav.html';
+import '../../ui/partials/Nav.html';
+
 
 import '../../ui/layouts/HomeLayout.html';
 import '../../ui/layouts/MainLayout.html';
 
 import '../../ui/pages/trips/TripsList.js';
+import '../../ui/pages/AboutMe.html';
 
 FlowRouter.route('/', {
   name: 'Home',
@@ -20,6 +23,15 @@ FlowRouter.route('/trips', {
   action() {
     BlazeLayout.render('MainLayout', {
       main: 'TripsList',
+    });
+  },
+});
+
+FlowRouter.route('/about', {
+  name: 'About me',
+  action() {
+    BlazeLayout.render('MainLayout', {
+      main: 'AboutMe',
     });
   },
 });
