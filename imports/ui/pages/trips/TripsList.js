060967377bc5e9f8b3677028c663d687f60b4f74
Tyran <turan.sergiy@gmail.com>
2017-02-23 21:16:40 +0200

    Subscribe for trips

--- imports/ui/pages/trips/TripsList.js
+++ imports/ui/pages/trips/TripsList.js
@@ -1,8 +1,21 @@
-import { Trips } from '../../../api/trips/trips.js'
+import { Trips } from '../../../api/trips/trips.js';
 
 import './Trip.html';
 import './TripsList.html';
 
+Template.TripsList.onCreated(function () {
+
+  var self = this;
+  self.autorun(function () {
+    self.subscribe('trips');
+  });
+
+});
+
 Template.TripsList.helpers({
+
+  trips() {
+    return Trips.find();
+  },
 
 });
