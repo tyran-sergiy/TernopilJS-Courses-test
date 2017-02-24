77f261bf6385fab3b1a12f142a20706b6c8ede6a
Tyran <turan.sergiy@gmail.com>
2017-02-24 20:52:10 +0200

    get random integer function

--- imports/ui/pages/sort/Sort.js
+++ imports/ui/pages/sort/Sort.js
@@ -1,6 +1,16 @@
 import './Sort.html';
-
+import { createArr } from '../../../modules/array_create.js';
 Template.Sort.events({
 
-  },
-});
+    'submit .sort'(event, instance) {
+
+      event.preventDefault();
+
+      const target = event.target;
+      const arrSize = target.size.value;
+      const condition = target.condition.value;
+
+      console.log(createArr(arrSize, condition));
+
+    },
+  });
